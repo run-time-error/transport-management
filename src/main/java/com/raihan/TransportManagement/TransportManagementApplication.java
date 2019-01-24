ffcfc652578892fd82237e87499f6b9a6a20b05e
Mostafa Khalid Raihan <mraihan29@gmail.com>
2019-01-24 20:49:35 +0600

    Development: Transport Repository added. Exception ongoing

---
+++ src/main/java/com/raihan/TransportManagement/TransportManagementApplication.java
@@ -0,0 +1,16 @@
+package com.raihan.TransportManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class TransportManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportManagementApplication.class, args);
+	}
+
+}
+
